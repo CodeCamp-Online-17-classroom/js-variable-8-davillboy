bda47e0af69e13037b76146ddeb60bee922195b4
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-21 23:16:41 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // write code here
+        let a = 0;
+        a++;
+        console.log("result:", a);        
+        a += 3;
+        console.log("result:", a);
+        a *= 17;
+        console.log("result:", a); 
+        a %= 7;
+        console.log("result:", a);
